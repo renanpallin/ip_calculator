d4ac45c55905d0e88a5a5a31209d63a26ef2c412
Renan Pallin <renanpallin@gmail.com>
2016-11-17 00:54:27 -0200

    Começando a arrumar estas funções

--- src/control/IpCalculator.java
+++ src/control/IpCalculator.java
@@ -3,8 +3,6 @@ package control;
 import java.util.ArrayList;
 import java.util.List;
 
-import control.IpCalculator.IpRages.IpRage;
-
 /**
  * Chamadas HTTP para java
  * http://pt.wikihow.com/Implementar-Requisi%C3%A7%C3%B5es-HTTP-POST-no-Android
@@ -113,25 +111,25 @@ public class IpCalculator{
 	 * @param ip in decimal system
 	 * @return the IP class or a char with 0 if an error occurs
 	 */
-	public char findIpClass(String dotedIp){
-		return findIpClass(divideOctets(dotedIp));
-	}
+//	public char findIpClass(String dotedIp){
+//		return findIpClass(divideOctets(dotedIp));
+//	}
 
-	public char findIpClass(long[] ip){
-		long firstOctect = ip[0];
-		if (firstOctect <= 127)
-			return 'A';
-		else if (firstOctect <= 191)
-			return 'B'; 
-		else if (firstOctect <= 239)
-			return 'C';
-		else if (firstOctect <= 239)
-			return 'D';
-		else if (firstOctect <= 254)
-			return 'E';
-		else
-			return '0'; // or throw new IllegalArgumentExcepton();
-	}
+//	public char findIpClass(long[] ip){
+//		long firstOctect = ip[0];
+//		if (firstOctect <= 127)
+//			return 'A';
+//		else if (firstOctect <= 191)
+//			return 'B'; 
+//		else if (firstOctect <= 239)
+//			return 'C';
+//		else if (firstOctect <= 239)
+//			return 'D';
+//		else if (firstOctect <= 254)
+//			return 'E';
+//		else
+//			return '0'; // or throw new IllegalArgumentExcepton();
+//	}
 
 	/**
 	 * Calculate the broadcast IP address for given IP and Mask
@@ -139,10 +137,10 @@ public class IpCalculator{
 	 * @param  mask [description]
 	 * @return      [description]
 	 */
-	public String calculateBroadcastIp(String dotedIp, String dotedMask){
-		int numSubnets = calculateNumOfSubnets(dotedMask);
-		return findRage(dotedIp, calculateIpRages(numSubnets)).getEnd();
-	}
+//	public String calculateBroadcastIp(String dotedIp, String dotedMask){
+//		int numSubnets = calculateNumOfSubnets(dotedMask);
+//		return findRage(dotedIp, calculateIpRages(numSubnets)).getEnd();
+//	}
 
 	/**
 	 * Calculate the subnet IP adress for given IP and Mask
@@ -150,15 +148,15 @@ public class IpCalculator{
 	 * @param  mask [description]
 	 * @return      [description]
 	 */
-	public long calculateSubnetIp(String dotedIp, String dotedMask){
-		int numSubnets = (int) calculateNumOfSubnets(dotedMask);
-		return findRage(dotedIp, calculateIpRages(numSubnets)).getStart();
-	}
-
-	public boolean isInTheSameSubnet(String dotedIp1, String dotedMask1, String dotedIp2, String dotedMask2){
-		if (!dotedMask1.equals(dotedMask2)) return false;
-		return calculateSubnetIp(dotedIp1, dotedMask1) == calculateSubnetIp(dotedIp2, dotedMask2);
-	}
+//	public long calculateSubnetIp(String dotedIp, String dotedMask){
+//		int numSubnets = (int) calculateNumOfSubnets(dotedMask);
+//		return findRage(dotedIp, calculateIpRages(numSubnets)).getStart();
+//	}
+//
+//	public boolean isInTheSameSubnet(String dotedIp1, String dotedMask1, String dotedIp2, String dotedMask2){
+//		if (!dotedMask1.equals(dotedMask2)) return false;
+//		return calculateSubnetIp(dotedIp1, dotedMask1) == calculateSubnetIp(dotedIp2, dotedMask2);
+//	}
 
 	/**
 	 * Descobre de qual rage o IP pertence 
@@ -166,14 +164,16 @@ public class IpCalculator{
 	 * @param  rages List of IpRages
 	 * @return       [description]
 	 */
-	private IpRage findRage(long[] ip, IpRages ipRages){
-		for (IpRage rage: ipRages.getRages()) {
-			if (ipw > rage.getStart() && ip < rage.getEnd()){
-				return rage;
-			}
-		}
-		return null;
-	}
+//	private IpRage findRage(long[] ip, IpRages ipRages){
+//		for (IpRage rage: ipRages.getRages()) {
+//			for (int i = 0; i < ip.length; i++){
+//				if (ip[i] > rage.getStart()[i] && ip[i] < rage.getEnd()[i]){
+//					return rage;
+//				}			
+//			}
+//		}
+//		return null;
+//	}
 	
 	/**
 	 * Calculate all the rages of Ip of the given mask
@@ -186,21 +186,21 @@ public class IpCalculator{
 	//
 	// 
 	//TODO: MÉTODO ERRADO! CONSERTAR ************************************************************************
-	private IpRages calculateIpRages(int numSubnets){
-		IpRages ipRages = new IpRages();
-
-		int hosts = 256 / numSubnets;
-		System.out.println(hosts);
-		int currentStart = 1;
-		int currentEnd = hosts - 2;
-		while (numSubnets-- > 0){
-		// for (int i = numSubnets; i >= 0; i--){
-			ipRages.addRage(currentStart, currentEnd);
-			currentStart += hosts;
-			currentEnd += hosts;
-		}
-		return ipRages;
-	}
+//	private IpRages calculateIpRages(int numSubnets){
+//		IpRages ipRages = new IpRages();
+//
+//		int hosts = 256 / numSubnets;
+//		System.out.println(hosts);
+//		int currentStart = 1;
+//		int currentEnd = hosts - 2;
+//		while (numSubnets-- > 0){
+//		// for (int i = numSubnets; i >= 0; i--){
+//			ipRages.addRage(currentStart, currentEnd);
+//			currentStart += hosts;
+//			currentEnd += hosts;
+//		}
+//		return ipRages;
+//	}
 /* END - Funções de cálculo de rede */
 
 
@@ -222,12 +222,12 @@ public class IpCalculator{
 /* END - Funções de cálculo e máscaras */
 
 
-	// public static void main(String[] args) {
-	// 	IpCalculator ipCalculator = new IpCalculator();
-	// 	for (IpRage rage : ipCalculator.calculateIpRages(4)) {
-	// 		System.out.println(rage.toString());
-	// 	}
-	// }
+//	 public static void main(String[] args) {
+//	 	IpCalculator ipCalculator = new IpCalculator();
+//	 	for (IpRage rage : ipCalculator.calculateIpRages(4)) {
+//	 		System.out.println(rage.toString());
+//	 	}
+//	 }
 
 
 	/**
@@ -269,21 +269,32 @@ public class IpCalculator{
 /* FUNÇÕES DE CONVERSÕES BÁSICAS - TODAS REFATORADAS! */
 	/**
 	 * REFATORADA PARA IP COMO ARRAY DE LONG
+	 * 
+	 * TODO: Zeros à esquerda no binário
 	 * Transform decimal given IP to binary IP
 	 * @param  ip [description]
 	 * @return    [description]
 	 */
-	private String decimalToBinaryIp(long[] ip){
+	public String decimalToBinaryIp(long[] ip){
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < ip.length; i++) {
-			sb.append(Long.toBinaryString(ip[i]));
+			String binaryOctet = Long.toBinaryString(ip[i]);
+			String binaryOctetEigthDigits = IpCalculator.zerosAEsquerdaRecursivo(binaryOctet);
+			
+			sb.append(binaryOctetEigthDigits);
 			if (i != ip.length-1) {
 				sb.append('.');
 			}
 		}
 		return sb.toString();
 	}
+	
+	public static void main(String[] args) {
+		IpCalculator ip = new IpCalculator();
+		System.out.println(ip.decimalToBinaryIp(new long[] {92, 0, 849, 788}));
+	}
 
+	
 	/**
 	 * NOVA FUNÇÃO!!
 	 *
@@ -344,66 +355,94 @@ public class IpCalculator{
 	 * @param  binaryMask Mask in binary way
 	 * @return boolean           [description]
 	 */
-	private boolean isMaskValid(String binaryMask){
-		/* Jeito complicado que comecei a fazer */
-		// char[] caracteres = binaryMask.replace(".","").toCharArray();
-		// boolean encontrouZero = false
-		// for (char c: caracteres) {
-		// 	if (c == '1'){
-		// 		if(encontrouZero){
-		// 			return false;
-		// 		}
-		// 	} else if(c == '0') {
-		// 		encontrouZero = true;
-		// 	} else {
-		// 		throw new IllegalArgumentException();
-		// 	}
-		// }
-		// return true;
-		 
-		/* Debug */
-		// System.out.println(binaryMask.split("0").length);
-		// for (String s : binaryMask.split("0")) {
-		// 	System.out.println(s);
-		// }
-		/* Jeito simples */
-		// System.out.println(binaryMask.split("0").length);
-		return binaryMask.split("0").length <= 1 && binaryMask.replace(".", "").length() == 8*4;
-	}
+//	private boolean isMaskValid(String binaryMask){
+//		/* Jeito complicado que comecei a fazer */
+//		// char[] caracteres = binaryMask.replace(".","").toCharArray();
+//		// boolean encontrouZero = false
+//		// for (char c: caracteres) {
+//		// 	if (c == '1'){
+//		// 		if(encontrouZero){
+//		// 			return false;
+//		// 		}
+//		// 	} else if(c == '0') {
+//		// 		encontrouZero = true;
+//		// 	} else {
+//		// 		throw new IllegalArgumentException();
+//		// 	}
+//		// }
+//		// return true;
+//		 
+//		/* Debug */
+//		// System.out.println(binaryMask.split("0").length);
+//		// for (String s : binaryMask.split("0")) {
+//		// 	System.out.println(s);
+//		// }
+//		/* Jeito simples */
+//		// System.out.println(binaryMask.split("0").length);
+//		return binaryMask.split("0").length <= 1 && binaryMask.replace(".", "").length() == 8*4;
+//	}
 
 /* END - Funções de validações */
 
+	
+	
+	
+/* Funções úteis TESTADAS */
+	/**
+	 * Overload to make 8 as default desired length
+	 * @param s
+	 * @return
+	 */
+	private static String zerosAEsquerdaRecursivo(String s){
+		return zerosAEsquerdaRecursivo(s, 8);
+	}
+	
+	/**
+	 * Fill with zeros in the left until desire length
+	 * @param s
+	 * @param tamanhoDesejado
+	 * @return
+	 */
+	private static String zerosAEsquerdaRecursivo(String s, int tamanhoDesejado){
+		int lenght = s.length();
+		if (lenght >= tamanhoDesejado)
+			return s;
+		else
+			return zerosAEsquerdaRecursivo("0" + s, tamanhoDesejado);
+	}
+	
+	
 
 /* DumbHelper(Experimental) */
 	/**
 	 * Encontra a máscara ideal para o número de hosts desejado
 	 * @param hosts [description]
 	 */
-	public String queroXHosts(int hosts){
-		StringBuilder binaryMaskForSubnet = new StringBuilder();
-		while(binaryMaskForSubnet.length() < 32)
-			binaryMaskForSubnet.append('1');
-
-		StringBuilder binaryMaskForHosts = new StringBuilder();
-		while (calculateNumOfHosts(binaryMaskForSubnet.toString() + binaryMaskForHosts.toString()) < hosts){
-			binaryMaskForSubnet.deleteCharAt(binaryMaskForSubnet.length() - 1);
-			binaryMaskForHosts.append('0');
-		}
-		return binaryMaskForSubnet.append(binaryMaskForHosts.toString()).toString();
-	}
-
-	public String queroXSubnets(int subnets){
-		StringBuilder binaryMaskForSubnet = new StringBuilder();
-		while(binaryMaskForSubnet.length() < 32)
-			binaryMaskForSubnet.append('1');
-
-		StringBuilder binaryMaskForHosts = new StringBuilder();
-		while (calculateNumOfSubnets(binaryMaskForSubnet.toString() + binaryMaskForHosts.toString()) < subnets){
-			binaryMaskForSubnet.deleteCharAt(binaryMaskForSubnet.length() - 1);
-			binaryMaskForHosts.append('0');
-		}
-		return binaryMaskForSubnet.append(binaryMaskForHosts.toString()).toString();
-	}
+//	public String queroXHosts(int hosts){
+//		StringBuilder binaryMaskForSubnet = new StringBuilder();
+//		while(binaryMaskForSubnet.length() < 32)
+//			binaryMaskForSubnet.append('1');
+//
+//		StringBuilder binaryMaskForHosts = new StringBuilder();
+//		while (calculateNumOfHosts(binaryMaskForSubnet.toString() + binaryMaskForHosts.toString()) < hosts){
+//			binaryMaskForSubnet.deleteCharAt(binaryMaskForSubnet.length() - 1);
+//			binaryMaskForHosts.append('0');
+//		}
+//		return binaryMaskForSubnet.append(binaryMaskForHosts.toString()).toString();
+//	}
+//
+//	public String queroXSubnets(int subnets){
+//		StringBuilder binaryMaskForSubnet = new StringBuilder();
+//		while(binaryMaskForSubnet.length() < 32)
+//			binaryMaskForSubnet.append('1');
+//
+//		StringBuilder binaryMaskForHosts = new StringBuilder();
+//		while (calculateNumOfSubnets(binaryMaskForSubnet.toString() + binaryMaskForHosts.toString()) < subnets){
+//			binaryMaskForSubnet.deleteCharAt(binaryMaskForSubnet.length() - 1);
+//			binaryMaskForHosts.append('0');
+//		}
+//		return binaryMaskForSubnet.append(binaryMaskForHosts.toString()).toString();
+//	}
 
 
 
--- test/test/IpCalculatorTest.java
+++ test/test/IpCalculatorTest.java
@@ -1,74 +1,85 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 import control.IpCalculator;
-import control.STring;
 
 public class IpCalculatorTest {
 
 	IpCalculator ipCalculator = new IpCalculator();
 	
 	@Test
-	public void testFindIpClassString() {
-		fail("Not yet implemented");
+	public void testDecimalToBinaryIp() {
+		IpCalculator ip = new IpCalculator();
+		assertEquals(ip.decimalToBinaryIp(new long[] {192, 168, 0, 1}), "11000000.10101000.00000000.00000001");
+		assertEquals(ip.decimalToBinaryIp(new long[] {175, 255, 4, 48}), "10101111.11111111.00000100.00110000");
+		assertEquals(ip.decimalToBinaryIp(new long[] {92, 0, 49, 88}), "01011100.00000000.00110001.01011000");
 	}
 	
-	@Test
-	public void testFindIpClassLongArray() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalculateBroadcastIp() {
-		assertEquals("Broadcast: ", ipCalculator.calculateBroadcastIp("143.116.158.164", "255.255.255.240"), "143.116.158.175"); // questão 4 Exercícios IP
-		assertEquals("Broadcast: ", ipCalculator.calculateBroadcastIp("172.31.136.0", "255.255.254.0"), "172.31.137.255");
-		assertEquals("Broadcast: ", ipCalculator.calculateBroadcastIp( "", ""), "");
-		
-	/*
-	 * "Pergunta: Quantas subredes e hosts por subrede pode-se obter usando a seguinte rede 172.27.0.0/26?
-		Resposta: 1024 subredes e 62 hosts"
-		"Pergunta: A qual subrede o host 172.31.57.221/23 pertece?
-		Resposta: 172.31.56.0"
-	 */
-		
-	}
-
-	@Test
-	public void testCalculateSubnetIp() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIsInTheSameSubnet() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalculateNumOfSubnetsLongArray() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalculateNumOfSubnetsString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalculateNumOfHosts() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testQueroXHosts() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testQueroXSubnets() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testFindIpClassString() {
+//		fail("Not yet implemented");
+//	}
+//	
+//	@Test
+//	public void testFindIpClassLongArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCalculateBroadcastIp() {
+////		assertEquals("Broadcast: ", ipCalculator.calculateBroadcastIp("143.116.158.164", "255.255.255.240"), "143.116.158.175"); // questão 4 Exercícios IP
+//////		assertEquals("Broadcast: ", ipCalculator.calculateBroadcastIp(new long[] {143,116,158,164}, "255.255.255.240"), "143.116.158.175"); // questão 4 Exercícios IP
+////		assertEquals("Broadcast: ", ipCalculator.calculateBroadcastIp("172.31.136.0", "255.255.254.0"), "172.31.137.255");
+////		assertEquals("Broadcast: ", ipCalculator.calculateBroadcastIp( "", ""), "");
+//		
+//	/*
+//	 * "Pergunta: Quantas subredes e hosts por subrede pode-se obter usando a seguinte rede 172.27.0.0/26?
+//		Resposta: 1024 subredes e 62 hosts"
+//		"Pergunta: A qual subrede o host 172.31.57.221/23 pertece?
+//		Resposta: 172.31.56.0"
+//	 */
+//		
+//	}
+//
+//	@Test
+//	public void testCalculateSubnetIp() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testIsInTheSameSubnet() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCalculateNumOfSubnetsLongArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCalculateNumOfSubnetsString() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCalculateNumOfHosts() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testQueroXHosts() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testQueroXSubnets() {
+//		fail("Not yet implemented");
+//	}
+	
+	
 
 }
